ccdc56e80727420eacc6bf75c5b8ece87be32ad8
userao <ostapenko2121@gmail.com>
2026-02-07 14:18:01 +0700

    changed deps for useEffect hook in Post component

--- src/pages/Post/Post.jsx
+++ src/pages/Post/Post.jsx
@@ -1,44 +1,44 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import style from "./Post.module.css";
 
 export default function Post() {
-    const { postId } = useParams();
-    const [post, setPost] = useState(null);
-    const [fetchState, setFetchState] = useState("");
+  const { postId } = useParams();
+  const [post, setPost] = useState(null);
+  const [fetchState, setFetchState] = useState("idle");
 
-    useEffect(() => {
-        const fetchPost = () => {
-            setFetchState("fetching");
-            fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`, {
-                method: "GET",
-                mode: "cors",
-            })
-                .then((res) => res.json())
-                .then((data) => {
-                    setPost(data);
-                    setFetchState("success");
-                })
-                .catch((err) => {
-                    setFetchState("error");
-                });
-        };
+  useEffect(() => {
+    const fetchPost = () => {
+      setFetchState("fetching");
+      fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`, {
+        method: "GET",
+        mode: "cors",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          setPost(data);
+          setFetchState("success");
+        })
+        .catch((err) => {
+          setFetchState("error");
+        });
+    };
 
-        fetchPost();
-    }, [post]);
+    fetchPost();
+  }, []);
 
-    console.log(post);
-    
+  console.log(fetchState);
 
-    const componentToRender = {
-        "fetching": () => <p>Загрузка поста</p>,
-        "error": () => <p>Ошибка сети</p>,
-        "success": () => <><h1>{post.title}</h1><p>{post.body}</p></>
-    }
+  const componentToRender = {
+    idle: () => null,
+    fetching: () => <p>Загрузка поста</p>,
+    error: () => <p>Ошибка сети</p>,
+    success: () => (
+      <>
+        <h1>{post.title}</h1>
+        <p>{post.body}</p>
+      </>
+    ),
+  };
 
-    return (
-        <article className="article">
-           {fetchState !== "" && componentToRender[fetchState]()} 
-        </article>
-    );
+  return <article className="article">{componentToRender[fetchState]()}</article>;
 }
